de6d4e61932c7d13d37cefe943739c2a2526b812
talhajutt-01 <talhaj928@gmail.com>
2024-01-17 19:45:00 +0500

    second

--- js/script.js
+++ js/script.js
@@ -68,6 +68,15 @@ const f7 = document.getElementById('f7');
 const f8 = document.getElementById('f8');
 const f9 = document.getElementById('f9');
 
+const g0 = document.getElementById('header');
+const g1 = document.getElementById('banner');
+const g2 = document.getElementById('category');
+const g3 = document.getElementById('computer');
+const g4 = document.getElementById('man');
+const g5 = document.getElementById('woman');
+const g6 = document.getElementById('footer');
+
+
 
 const img1 = document.getElementById('img1');
 const img2 = document.getElementById('img2');
@@ -103,6 +112,10 @@ function updateContent(element, newValue) {
   function updateImageSrc(imageElement, newSrc) {
     // Update the src attribute of the image element
     imageElement.src = newSrc;
+}
+function updateBackgroundColor(element, newColor) {
+  // Update the background color for the element
+  element.style.backgroundColor = newColor;
 }
 
   
@@ -181,6 +194,15 @@ function updateContent(element, newValue) {
             updateContent(f7, jsonData.footer.f7);
             updateContent(f8, jsonData.footer.f8);
             updateContent(f9, jsonData.footer.f9);
+
+            updateBackgroundColor(g0, jsonData.color.header);
+            updateBackgroundColor(g1, jsonData.color.banner);
+            updateBackgroundColor(g2, jsonData.color.category);
+            updateBackgroundColor(g3, jsonData.color.computer);
+            updateBackgroundColor(g4, jsonData.color.man);
+            updateBackgroundColor(g5, jsonData.color.woman);
+            updateBackgroundColor(g6, jsonData.color.footer);
+        
 
             updateImageSrc(img1, jsonData.images.img1);
             updateImageSrc(img2, jsonData.images.img2);
